7a953c53e1cf92dec51a13274396d61f21dc6abc
Thiago Moura <76569184+ThiagoMoura963@users.noreply.github.com>
2024-04-17 17:26:13 -0300

    Add files via upload

---
+++ forca.h
@@ -0,0 +1,13 @@
+#define TAMANHO_PALAVRA 20
+
+void abertura(); 
+void chuta();
+int jachutou(char letra);
+void desenhaforca();
+void escolhepalavra();
+void escolhepalavra();
+int enforcou();
+int acertou();
+void escolhepalavra();
+void adicionapalavra();
+int chuteserrados();
